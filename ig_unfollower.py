79cfb963c0a21a5ee15b549bea282e0020a2d6f8
johnSMUpark <park@smu.edu>
2019-12-29 16:49:23 -0800

    w

--- ig_unfollower.py
+++ ig_unfollower.py
@@ -21,7 +21,7 @@ class IG_Unfollower(object):
         self.following   = set()
         self.followers   = set()
         self.unfollowers = set()
-        self.whitelist   = ['espn', 'abcnews', 'pbs'] # list of accounts to not unfollow
+        self.whitelist   = ['espn', 'abcnews', 'pbs'] # list of accounts NOT to unfollow
 
     def login(self):
         print('Logging in...')
@@ -46,7 +46,6 @@ class IG_Unfollower(object):
 
     def get_following(self): 
         self.driver.get(f'https://www.instagram.com/{self.username}/')
-        #self.wait_by_class_name('-nal3 ')
         self.wait_by_css('a.-nal3')
         #self.wait_by_xpath("//button[text()='Not Now']")
 
@@ -60,64 +59,60 @@ class IG_Unfollower(object):
     def get_followers(self):    
         self.driver.get(f'https://www.instagram.com/{self.username}/')
         self.wait_by_css('a.-nal3')
-        #self.wait_by_class_name('-nal3 ')
-        #self.wait_by_xpath("//button[text()='Not Now']")
 
         follower_btn = self.driver.find_element_by_css_selector(f"[href='/{self.username}/followers/']")
-        #follower_btn = self.driver.find_element_by_css_selector("[class='-nal3 '][href='/ben.alfred.johnson/followers/']")
-        #follower_btn = self.driver.find_element_by_xpath("//a[class='-nal3 ' and text()='followers']")
         follower_btn.click()
 
         print('Gathering your followers...')
         self.scroll_and_collect_users(self.followers)
 
     def scroll_and_collect_users(self, gathered_users):
-        time.sleep(randrange(3)+1)
-        scrollable = self.driver.find_element_by_css_selector('div.isgrP')
-        last_height = self.driver.execute_script('return arguments[0].scrollHeight', scrollable)
+        time.sleep(randrange(3)+1) # wait a bit
+        scrollable = self.driver.find_element_by_css_selector('div.isgrP') # find scrollable window
+        last_height = self.driver.execute_script('return arguments[0].scrollHeight', scrollable) # track scrollable window's height
         while(1):
-            time.sleep(randrange(3)+1)
-            collected_users = self.driver.find_elements_by_css_selector('a.FPmhX.notranslate._0imsa')
-            self.driver.execute_script('arguments[0].scrollIntoView()', collected_users[-1])
-            time.sleep(randrange(3)+1)
-            new_height = self.driver.execute_script('return arguments[0].scrollHeight', scrollable)
-            if new_height == last_height: break
-            last_height = new_height
-        for user in collected_users: gathered_users.add(user.text)
+            time.sleep(randrange(3)+1) # wait a bit
+            collected_users = self.driver.find_elements_by_css_selector('a.FPmhX.notranslate._0imsa') # collect users
+            self.driver.execute_script('arguments[0].scrollIntoView()', collected_users[-1]) # scroll down to last user collected
+            time.sleep(randrange(3)+1) # wait for more users to lead
+            new_height = self.driver.execute_script('return arguments[0].scrollHeight', scrollable) # new height of scrollable window
+            if new_height == last_height: break # if you can't scroll down any further, exit
+            last_height = new_height # update height
+        for user in collected_users: gathered_users.add(user.text) # update set with gathered users
 
     def unfollow(self):
         print('Gathering unfollowers...')
-        self.unfollowers = self.following - self.followers
+        self.unfollowers = self.following - self.followers # unfollowers = set of following - set of followers
 
         print('You have ' + str(len(self.unfollowers)) + ' unfollowers.')
         if(len(self.unfollowers)>100):        # if num unfollowers > 100
-            while(len(self.unfollowers)>100): # cap accounts unfollowed
-                self.unfollowers.pop()        # to 100
+            while(len(self.unfollowers)>100): # cap accounts unfollowed at 100
+                self.unfollowers.pop()
 
         print('Unfollowing users...')        
-        for unfollower in self.unfollowers:
-            if unfollower not in self.whitelist:
-                self.driver.get(f'https://www.instagram.com/{unfollower}/')
-                self.wait_by_xpath("//button[text()='Following']")
+        for unfollower in self.unfollowers: # for each unfollower
+            if unfollower not in self.whitelist: # if unfollower is not in whitelist
+                self.driver.get(f'https://www.instagram.com/{unfollower}/') # go to unfollower's profile page
+                self.wait_by_xpath("//button[text()='Following']") # wait for Following button to load
                 
                 time.sleep(randrange(2)+1)
                 follow_btn = self.driver.find_element_by_xpath("//button[text()='Following']")
-                follow_btn.click()
+                follow_btn.click() # locate & click unfollow btn
 
                 time.sleep(randrange(2)+1)
                 confirm_unfollow_btn = self.driver.find_element_by_css_selector('button.aOOlW.-Cab_')
-                confirm_unfollow_btn.click()
+                confirm_unfollow_btn.click() # locate & click confirm unfollow btn
 
     # wait functions
     def wait_by_class_name(self, class_name): # wait 3 sec or until class_name appears
         try: WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
         except TimeoutException: print('Could not find ' + class_name) # print if takes too long
     
-    def wait_by_css(self, css):
+    def wait_by_css(self, css): # wait 3 sec or until css_label appears
         try: WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, css)))
         except TimeoutException: print('Could not find ' + css) # print if takes too long
 
-    def wait_by_xpath(self, path): # wait 3 sec or until class_name appears
+    def wait_by_xpath(self, path): # wait 3 sec or until xpath appears
         try: WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, path)))
         except TimeoutException: print('Could not find ' + path) # print if takes too long
 
